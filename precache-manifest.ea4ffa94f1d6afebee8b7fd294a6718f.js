4a694c022dc7843c08e966fcdd32ce7aa924ae74
unknown <Vijay Kumar>
2019-09-19 00:05:05 +0530

    Updates

---
+++ precache-manifest.ea4ffa94f1d6afebee8b7fd294a6718f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d0e636b6225167b9f6b774b2a0055d5",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "5a02e682716180aa089d",
+    "url": "/contactmanager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "b437de9e7dceaa00a69e",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "5a02e682716180aa089d",
+    "url": "/contactmanager/static/js/2.f0753790.chunk.js"
+  },
+  {
+    "revision": "b437de9e7dceaa00a69e",
+    "url": "/contactmanager/static/js/main.898ca9f7.chunk.js"
+  },
+  {
+    "revision": "b6597f31777016aa759a",
+    "url": "/contactmanager/static/js/runtime~main.77e22976.js"
+  }
+]);
